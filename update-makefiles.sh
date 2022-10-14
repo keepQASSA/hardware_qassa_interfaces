eefe9623f202408d6329b8f3a092673604187fb5
ghou8s <ghou8s@gmail.com>
2022-10-14 23:17:05 +0000

    Revert "Remove lineage power hal"
    This reverts commit 71685577d081e042a14c943d8c0a745941e617ef.

--- update-makefiles.sh
+++ update-makefiles.sh
@@ -3,5 +3,6 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
+  "vendor.lineage:hardware/custom-interfaces" \
   "android.hardware:hardware/interfaces" \
   "android.hidl:system/libhidl/transport"
